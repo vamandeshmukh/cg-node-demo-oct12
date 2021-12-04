efdb717c73b07fc6fa75611aba60c58c522a931b
vamandeshmukh <vamandeshmukh@gmail.com>
2021-12-04 12:25:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,26 @@ const express = require('express');
 const app = express();
 const port = 8084;
 
-app.get('/',(request, response) => {
-    response.send(`Welcome to my forst node app.`);
+app.get('/', (request, response) => {
+    response.send(`Welcome to my first node app.`);
     console.log(`Welcome`);
-
 });
+
+app.get('/home', (request, response) => {
+    response.send(`Welcome to home page.`);
+    console.log(`home`);
+});
+
+app.get('/login', (request, response) => {
+    response.send(`Welcome to login.`);
+    console.log(`login`);
+});
+
 app.listen(port, () => {
     console.log(`App is running...`);
-
 });
 
 // go to CMD and run - 
 // NPM start 
+// go to browser and open 
+// http://localhost:8084/ 
