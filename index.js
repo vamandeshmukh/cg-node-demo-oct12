1ba9ee548183001dc86ae302f6ed0447382e8102
vamandeshmukh <vamandeshmukh@gmail.com>
2021-12-04 12:07:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+const express = require('express');
+const app = express();
+const port = 8084;
+
+app.get((request, response) => {
+    response.send(`Welcome to my forst node app.`);
+    console.log(`Welcome`);
+
+});
+app.listen(port, () => {
+    console.log(`App is running...`);
+
+});
