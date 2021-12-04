1a944e7f5e519629701568f4f01cf0c10834ece3
vamandeshmukh <vamandeshmukh@gmail.com>
2021-12-04 22:33:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,22 @@
 const express = require('express');
 const app = express();
 const port = 8084;
+const axios = require('axios');
 
 app.get('/', (request, response) => {
     response.send(`Welcome to my first node app.`);
     console.log(`Welcome`);
+});
+
+app.get('/emp', (request, response) => {
+    axios.get('http://localhost:8082/getallemps')
+        .then((res) => {
+            response.send(res.data);
+        })
+        .catch((err) => {
+            response.send(err);
+        });
+    console.log(`emp`);
 });
 
 app.get('/home', (request, response) => {
